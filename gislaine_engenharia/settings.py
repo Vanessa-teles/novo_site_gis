89ea4cde8b82a814ec227391501308f794058043
Vanessa-teles <soymichefa001@gmail.com>
2025-06-13 22:42:14 -0300

    DEBUG = False

--- gislaine_engenharia/settings.py
+++ gislaine_engenharia/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gislaine-teles-engenharia-diagnostica-2025'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
