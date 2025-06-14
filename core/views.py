2ec9efd2a3dba7aad50b04c0665a2f62a8474c04
Vanessa-teles <soymichefa001@gmail.com>
2025-06-14 00:55:10 -0300

    email2

--- core/views.py
+++ core/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.urls import reverse
 from .forms import ContatoForm
-
-def index(request):
-    return render(request, 'core/index.html')
 
 def contato(request):
     if request.method == 'POST':
         form = ContatoForm(request.POST)
         if form.is_valid():
             try:
-                form.sendEmail()
-                messages.success(request, 'E-mail enviado com sucesso!')
-                return redirect(reverse('contato'))
+                # Chamando o método send_email (não send_mail)
+                form.send_email()
+                return redirect('pagina_de_sucesso')  # Redireciona após envio
             except Exception as e:
-                messages.error(request, f'Erro ao enviar e-mail: {str(e)}')
-                return redirect(reverse('contato'))
-    return render(request, 'core/contato.html')
-
-from django.core.mail import send_mail, EmailMessage
+                # Adiciona erro ao formulário para mostrar ao usuário
+                form.add_error(None, f'Erro ao enviar mensagem: {str(e)}')
+    else:
+        form = ContatoForm()
+    
+    return render(request, 'seu_template.html', {'form': form})
